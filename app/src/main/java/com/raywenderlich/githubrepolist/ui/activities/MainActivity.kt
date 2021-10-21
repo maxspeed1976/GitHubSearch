3062742ec5234225e395bdb7167207c974244a3c
Oleg Kremliakov <olegdnepr@gmail.com>
2021-10-21 15:22:44 +0300

    Working branch without Retrofit

---
+++ app/src/main/java/com/raywenderlich/githubrepolist/ui/activities/MainActivity.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2018 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.githubrepolist.ui.activities
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Bundle
+
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.raywenderlich.githubrepolist.R
+import com.raywenderlich.githubrepolist.data.RepoResult
+import com.raywenderlich.githubrepolist.data.Request
+import com.raywenderlich.githubrepolist.ui.adapters.RepoListAdapter
+import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers.Main
+
+
+class MainActivity : Activity() {
+
+   /* private val items = listOf(
+        "JetBrains/kotlin - The Kotlin Programming Language",
+        "exercism/kotlin - Exercism exercises in Kotlin",
+        "cbeust/kobalt - A Kotlin-based build system for the JVM",
+        "JetBrains/kotlin - The Kotlin Programming Language",
+        "exercism/kotlin - Exercism exercises in Kotlin",
+        "cbeust/kobalt - A Kotlin-based build system for the JVM",
+        "JetBrains/kotlin - The Kotlin Programming Language"
+    )*/
+
+    @DelicateCoroutinesApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val repoList = findViewById<RecyclerView>(R.id.repoList)
+        repoList.layoutManager = LinearLayoutManager(this)
+      //  repoList.adapter = RepoListAdapter()
+
+        val url =
+            "https://api.github.com/search/repositories?q=mario+language:kotlin&sort=stars&order=desc"
+        if (isNetworkConnected()) {
+
+                GlobalScope.async(Dispatchers.Default) {
+                    // do background work
+                    val result=Request().run()
+                    withContext(Main) {
+                       repoList.adapter = RepoListAdapter(result)
+                    }
+                }
+        } else {
+            AlertDialog.Builder(this).setTitle("No internet connection")
+                .setMessage("Please check your internet connection and try again")
+                .setPositiveButton(android.R.string.ok) {_,_ ->}
+                .setIcon(android.R.drawable.ic_dialog_alert).show()
+
+
+        }
+
+
+    }
+
+    private fun isNetworkConnected(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as
+                ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+        return networkCapabilities != null &&
+                networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+
+    }
+}
